374826b08229c01f4f859e3479e8762f46cb2b52
ATRIKA SHOW <readeratrika08@gmail.com>
2023-08-28 11:04:45 +0530

    Job sequencing problem in O(nlogn) TC

--- ClassQuestions/JobSequencingProblem.java
+++ ClassQuestions/JobSequencingProblem.java
@@ -1,28 +1,55 @@
 import java.util.*;
 public class JobSequencingProblem {
 
+    static class Job{
+        int idx;
+        int deadline;
+        int profit;
+
+        Job(int i, int d, int p){
+            this.idx = i;
+            this.deadline = d;
+            this.profit = p;
+        }
+    }
+
     // Time Complexity => O(nlogn)
     public static int maxProfit(int jobs[][]){
-        // I've to sort this array based on profit
-        Arrays.sort(jobs, Comparator.comparingDouble(o -> o[1])); // accending
+        ArrayList<Job> jobsInfo = new ArrayList<>();
 
-        int finalProfit = 0;
-        int time =0;
+        for (int i = 0; i < jobs.length; i++) {
+            jobsInfo.add(new Job(i, jobs[i][0], jobs[i][1]));
+        }
 
-        // select first job as it will give max profit
-        finalProfit += jobs[jobs.length-1][1];
-        time++;
-        for (int i = jobs.length-2; i >= 0; i--) {
-            if (jobs[i][0] > time) { // select the job as deadline hasn't passed
-                finalProfit += jobs[i][1];
-                time++;
+        // object sorting
+        Collections.sort(jobsInfo, (obj1,obj2) -> obj2.profit-obj1.profit);    // decending order
+
+        // To store the index of selected job
+        ArrayList<Integer> selectedJob = new ArrayList<>();
+
+        int Time = 0;
+        int maxProfit =0;
+        
+        for (int i = 0; i < jobs.length; i++) {
+            Job currJob = jobsInfo.get(i);   // it will give me id, deadline & profit of my current job
+            if (currJob.deadline > Time) {  // still I've time to complete this job
+                maxProfit += currJob.profit;
+                selectedJob.add(currJob.idx);        // adding idx of selected job
+                Time++;
             }
         }
-        return finalProfit;
+
+        int ans[] = new int[2];
+        ans[0] = selectedJob.size(); // no of job can be done
+        ans[1] = maxProfit;         // maxprofit
+        for (int i = 0; i < ans.length; i++) {
+            System.out.println(ans[i] );
+        }
+        return maxProfit;
     }
 
     public static void main(String[] args) {
         int jobs[][] = {{4,20},{1,10},{1,40},{1,30}};
-        System.out.println(maxProfit(jobs));
+        maxProfit(jobs);
     }
 }
